db6b2df61fc6cfeef71540a23733026f54e55730
Paulo Fernando <61089612+Paulo-Fernando-R@users.noreply.github.com>
2022-05-16 12:02:27 -0300

    .

--- src/pages/teste/index.js
+++ src/pages/teste/index.js
@@ -4,5 +4,18 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-      <h1>TESTE</h1>
+
+    <div className='back'>
+         <div className='header'>
+            <Image height='20' width='20' src='/images/gota-de-sangue.png'/>
+            <Image height='20' width='20' src='/images/dry-clean.png'/>
+        </div>
+
+        <nav>
+            
+        </nav>
+    </div>
+   
+
+    
   )}
